5420c1c40efdb04a55f4ef092e6cbdbf113c7a15
rohankumar404 <rl.cse.rohan@gmail.com>
2026-04-19 23:40:53 +0530

    fix email temlate with the company details and logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,13 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Mapsily Tools' || trim($slot) === 'Laravel')
-<img src="{{ asset('assets/img/mapsily-logo.png') }}" class="logo" alt="Mapsily Tools Logo" style="max-height: 45px; object-fit: contain;">
-@else
-{!! $slot !!}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Mapsily Tools' || trim($slot) === 'Laravel')
+                <img src="https://mapsily.com/wp-content/uploads/2026/01/mapsily-logo-scaled.png" class="logo"
+                    alt="Mapsily Tools Logo" style="object-fit: contain;">
+            @else
+                {!! $slot !!}
+            @endif
+        </a>
+    </td>
 </tr>
